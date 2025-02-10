98624d3a077d2e255329118d3edc1baed10888eb
derticksg <maksromanovskij281@gmail.com>
2025-02-10 22:04:36 +0300

    Add files via upload

---
+++ massaobrabotka (1).ino
@@ -0,0 +1,32 @@
+#include <Servo.h>
+
+#define pinServo3 3
+#define pinServo10 10
+int shaftPos = 0;
+
+Servo moment;
+Servo number;
+
+void setup()
+{
+  moment.attach(pinServo3);
+  number.attach(pinServo10);
+}
+
+void loop() {
+  
+  for (shaftPos = 0; shaftPos <= 180; shaftPos ++) { 
+  
+    moment.write(shaftPos);
+    number.write(shaftPos);           
+    delay(15); 
+  }
+
+  for (shaftPos = 180; shaftPos >= 0; shaftPos --) { 
+  
+    moment.write(shaftPos);
+    number.write(shaftPos);           
+    delay(15); 
+  }
+}
+
---
+++ massaobrabotka.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+#define pin10 10
+#define pin3 3
+Servo gate1, gate2;
+void setup(){
+  gate1.attach(pin10);
+  gate2.attach(pin3);
+}
+
+void loop(){
+  gate1.write(0);
+  gate2.write(0);
+  delay(1000);
+  gate1.write(90);
+  gate2.write(90);
+  delay(1000);
+  gate1.detach();
+  gate2.detach();
+  }
+
